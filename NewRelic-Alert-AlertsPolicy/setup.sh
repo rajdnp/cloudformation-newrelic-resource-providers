9a3fb59b00cb0fe8f20c093cfde8aa228662aaab
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-20 12:11:10 -0800

    Adding NewRelic::AlertPolicy to CICD

--- NewRelic-Alert-AlertsPolicy/setup.sh
+++ NewRelic-Alert-AlertsPolicy/setup.sh
@@ -3,7 +3,7 @@
 # Set up any prerequisites needed for cfn test
 #
 mkdir -p inputs
-cat example_inputs/inputs_1_create.json | sed "s/ACCOUNT_ID/${ACCOUNT_ID}/g" > inputs/inputs_1_create.json
-cat example_inputs/inputs_1_update.json  | sed "s/ACCOUNT_ID/${ACCOUNT_ID}/g" > inputs/inputs_1_update.json
-cat test/integ-template.yml | sed "s/ACCOUNT_ID/${ACCOUNT_ID}/g" > test/integ.yml
+cat example_inputs/inputs_1_create.json | sed "s/NEWRELIC_ACCOUNT_ID/${NEWRELIC_ACCOUNT_ID}/g" > inputs/inputs_1_create.json
+cat example_inputs/inputs_1_update.json  | sed "s/NEWRELIC_ACCOUNT_ID/${NEWRELIC_ACCOUNT_ID}/g" > inputs/inputs_1_update.json
+cat test/integ-template.yml | sed "s/NEWRELIC_ACCOUNT_ID/${NEWRELIC_ACCOUNT_ID}/g" > test/integ.yml
 
